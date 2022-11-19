4534f276620a05cb05706a24c13e9b7ef03cadb9
i.bakumtsev <i.bakumtsev@cdek.ru>
2022-11-19 18:35:41 +0300

    master: failed commit [2]

--- src/test/java/com/bakumcev/demo/service/GitHubServiceTest.java
+++ src/test/java/com/bakumcev/demo/service/GitHubServiceTest.java
@@ -44,7 +44,7 @@ class GitHubServiceTest extends AbstractSpringIntegrationTest {
 
         var result = gitServiceSpy.push();
 
-        assertEquals(LAST_COMMIT_ALREADY.getCode(), result);
+        assertEquals("1", result);
     }
 
     @Test
