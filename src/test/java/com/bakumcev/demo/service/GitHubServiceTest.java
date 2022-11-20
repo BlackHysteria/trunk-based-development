a116ebc157c8c14c5bbd7988fc8970ffc2530474
i.bakumtsev <i.bakumtsev@cdek.ru>
2022-11-20 18:26:46 +0300

    master: more refactoring [5]

--- src/test/java/com/bakumcev/demo/service/GitHubServiceTest.java
+++ src/test/java/com/bakumcev/demo/service/GitHubServiceTest.java
@@ -42,7 +42,7 @@ class GitHubServiceTest extends AbstractSpringIntegrationTest {
 
         var result = gitServiceSpy.push();
 
-        assertEquals(COMMIT_PUSHED.getCode(), result);
+        assertEquals("1", result);
     }
 
     @Test
